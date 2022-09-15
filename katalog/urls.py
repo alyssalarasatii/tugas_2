e1209555c833c0246c9215f4d721eea5ae65abb1
alyssa.larasati <alyssa.larasati@ui.ac.id>
2022-09-15 22:33:46 +0700

    push sementara

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,1 +1,9 @@
 # TODO: Implement Routings Here
+from django.urls import path
+from katalog.views import s_katalog
+
+app_name = 'katalog'
+
+urlpatterns = [
+    path('', s_katalog, name='s_katalog'),
+]
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
+from tugas_2.katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def s_katalog(request):
+    barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_katalog': barang_katalog,
+        'nama': 'Alyssa Larasati',
+        'student_id': '2106656125'
+    }
+    return render(request, "katalog.html",context)
