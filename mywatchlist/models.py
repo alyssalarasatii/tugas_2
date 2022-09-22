330b3819bfd2fb2862944f0dbdfdd10300e4c381
alyssa.larasati <alyssa.larasati@ui.ac.id>
2022-09-22 16:55:05 +0700

    larasss

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class MyWatchList(models.Model):
-    watched = models.TextField()
-    title = models.TextField()
+    watched = models.TextField(default='')
+    title = models.TextField(default='')
     rating = models.CharField(max_length=255)
     release_date = models.CharField(max_length=30)
-    review = models.CharField(max_length=255)
+    review = models.TextField(default='')
